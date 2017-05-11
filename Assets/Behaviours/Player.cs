41e879585a0a0724f968424b9081d8606a57a0bd
Jdasi <joseph2.dasilva@live.uwe.ac.uk>
2017-05-11 17:18:51 +0100

    added basic movement and a floor plane

---
+++ Assets/Behaviours/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int id = 0;
+
+    public float move_speed = 20;
+
+    private float horizontal = 0;
+    private float vertical = 0;
+
+	void Start()
+    {
+		
+	}
+	
+	void Update()
+    {
+		horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * move_speed;
+        vertical = Input.GetAxis("Vertical") * Time.deltaTime * move_speed;
+
+        transform.position += new Vector3(horizontal, 0, vertical);
+	}
+}
