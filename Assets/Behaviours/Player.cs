fd1f6a8b763480333cce35b6e72f6c8b33f38291
Jdasi <joseph2.dasilva@live.uwe.ac.uk>
2017-05-11 19:43:54 +0100

    changed the way mount points work; player screen is now a canvas

--- Assets/Behaviours/Player.cs
+++ Assets/Behaviours/Player.cs
@@ -7,25 +7,11 @@ public class Player : MonoBehaviour
     public int id = 0;
     public float move_speed = 20;
 
-    public GameObject test_eyes;
-
-    private Dictionary<MountPoint.ID, MountPoint> mount_points = new Dictionary<MountPoint.ID, MountPoint>();
-    private GameObject mount_origin;
+    public List<GameObject> mount_points = new List<GameObject>();
 
 	void Start()
     {
-        mount_origin = transform.FindChild("Body").gameObject;
 
-        MountPoint mount_point = new MountPoint();
-        mount_point.mount_object = Instantiate(test_eyes, transform.position, Quaternion.identity, transform) as GameObject;
-        mount_point.mount_offset = new Vector3(0, 1, 0);
-        
-        mount_points.Add(MountPoint.ID.eyes, mount_point);
-
-		Vector3 look_pos = Camera.main.transform.position - transform.position;
-        look_pos.x = 0;
-
-        transform.LookAt(look_pos);
 	}
 	
 
@@ -35,25 +21,5 @@ public class Player : MonoBehaviour
         float vertical = Input.GetAxis("Vertical") * Time.deltaTime * move_speed;
 
         transform.position += new Vector3(horizontal, 0, vertical);
-
-        UpdateMountPoints();
 	}
-
-
-    void UpdateMountPoints()
-    {
-        foreach (var elem in mount_points)
-        {
-            GameObject mount_object = elem.Value.mount_object;
-            Vector3 mount_offset = elem.Value.mount_offset;
-
-            if (mount_object == null)
-            {
-                continue;
-            }
-
-            mount_object.transform.position = mount_origin.transform.position;
-            mount_object.transform.localPosition += mount_offset;
-        }
-    }
 }
