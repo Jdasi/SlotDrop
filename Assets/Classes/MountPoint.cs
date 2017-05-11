d9e30d902f294a8299ff12e8197107472c92610c
Jdasi <joseph2.dasilva@live.uwe.ac.uk>
2017-05-11 18:41:34 +0100

    added mount points for player

---
+++ Assets/Classes/MountPoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MountPoint
+{
+    public enum ID
+    {
+        hat,
+        face,
+        eyes,
+        torso,
+        hand
+    }
+
+    public GameObject mount_object;
+    public Vector3 mount_offset;
+}
