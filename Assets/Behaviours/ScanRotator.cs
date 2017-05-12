63f2b3293a2ce147474317bfeba794d713b9d478
Tom Turner <tom3.turner@live.uwe.ac.uk>
2017-05-12 20:31:37 +0100

    Pc Manager class

---
+++ Assets/Behaviours/ScanRotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScanRotator : MonoBehaviour
+{
+    RectTransform rect_transform;
+    public float rotation_speed = 5f;
+	// Use this for initialization
+	void Start ()
+    {
+        rect_transform = GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        rect_transform.Rotate(Vector3.forward * Time.deltaTime * rotation_speed);
+    }
+}
