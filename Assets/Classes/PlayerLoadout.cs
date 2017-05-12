b760ea51706c8b3e8eaa42f72fed0a37272c25b2
med1337 <leszek.bagnucki@gmail.com>
2017-05-12 17:59:33 +0100

    loadouts

---
+++ Assets/Classes/PlayerLoadout.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Assets.Classes
+{
+    public struct PlayerLoadout
+    {
+        public LoadoutType LoadoutTypes;
+        public WeaponType BasePlayerWeapon;
+        public WeaponType SpecialPlayerWeapon;
+    }
+}
