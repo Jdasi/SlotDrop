e847b5f75459659ac0a32fff30992ed63caec4cc
Jdasi <joseph2.dasilva@live.uwe.ac.uk>
2017-05-12 11:46:36 +0100

    started player spawning stuff

---
+++ Assets/Behaviours/ControllerManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class ControllerManager : MonoBehaviour
+{
+    void Awake()
+    {
+        // Listen for controller connection events
+        ReInput.ControllerConnectedEvent += OnControllerConnected;
+
+        // Assign each Joystick to a Player initially
+        foreach (Joystick joystick in ReInput.controllers.Joysticks)
+        {
+            if (ReInput.controllers.IsJoystickAssigned(joystick))
+                continue; // Joystick is already assigned
+
+            // Assign Joystick to first Player that doesn't have any assigned
+            AssignJoystickToNextOpenPlayer(joystick);
+        }
+    }
+
+
+    void OnControllerConnected(ControllerStatusChangedEventArgs args)
+    {
+        if (args.controllerType != ControllerType.Joystick)
+            return; // skip if this isn't a Joystick
+
+        AssignJoystickToNextOpenPlayer(ReInput.controllers.GetJoystick(args.controllerId));
+    }
+
+
+    void AssignJoystickToNextOpenPlayer(Joystick joystick)
+    {
+        foreach (Player player in ReInput.players.Players)
+        {
+            if (player.controllers.joystickCount > 0)
+                continue; // player already has a joystick
+
+            player.controllers.AddController(joystick, true); // assign joystick to player
+
+            return; // joystick assigned, abort
+        }
+    }
+
+
+    void OnDestroy()
+    {
+        // Unsubscribe from events
+        ReInput.ControllerConnectedEvent -= OnControllerConnected;
+
+        //ReInput.ControllerDisconnectedEvent -= OnControllerDisconnected;
+        //ReInput.ControllerPreDisconnectEvent -= OnControllerPreDisconnect;
+    }
+}
--- Assets/Behaviours/PlayerManager.cs
+++ Assets/Behaviours/PlayerManager.cs
@@ -1,58 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Rewired;
 
 public class PlayerManager : MonoBehaviour
 {
+    const int MAX_PLAYERS = 32;
+
+    public Transform player_spawn;
     public GameObject player_prefab;
 
-    void Awake()
+	void Start()
     {
-        // Listen for controller connection events
-        ReInput.ControllerConnectedEvent += OnControllerConnected;
-
-        // Assign each Joystick to a Player initially
-        foreach (Joystick joystick in ReInput.controllers.Joysticks)
+		
+	}
+	
+	void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (ReInput.controllers.IsJoystickAssigned(joystick))
-                continue; // Joystick is already assigned
-
-            // Assign Joystick to first Player that doesn't have any assigned
-            AssignJoystickToNextOpenPlayer(joystick);
+            //Instantiate(player_prefab, 
         }
-    }
-
-
-    void OnControllerConnected(ControllerStatusChangedEventArgs args)
-    {
-        if (args.controllerType != ControllerType.Joystick)
-            return; // skip if this isn't a Joystick
-
-        AssignJoystickToNextOpenPlayer(ReInput.controllers.GetJoystick(args.controllerId));
-    }
-
-
-    void AssignJoystickToNextOpenPlayer(Joystick joystick)
-    {
-        foreach (Player player in ReInput.players.Players)
-        {
-            if (player.controllers.joystickCount > 0)
-                continue; // player already has a joystick
-
-            player.controllers.AddController(joystick, true); // assign joystick to player
-
-            return; // joystick assigned, abort
-        }
-    }
-
-
-    void OnDestroy()
-    {
-        // Unsubscribe from events
-        ReInput.ControllerConnectedEvent -= OnControllerConnected;
-
-        //ReInput.ControllerDisconnectedEvent -= OnControllerDisconnected;
-        //ReInput.ControllerPreDisconnectEvent -= OnControllerPreDisconnect;
-    }
+	}
 }
