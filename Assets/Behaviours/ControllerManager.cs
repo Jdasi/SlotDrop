f7595bb3e3ed066feaa75d9b863e5debf03e01ae
Tom Turner <tom3.turner@live.uwe.ac.uk>
2017-05-12 13:07:12 +0100

    shit didn't commit control manger

--- Assets/Behaviours/ControllerManager.cs
+++ Assets/Behaviours/ControllerManager.cs
@@ -5,52 +5,122 @@ using Rewired;
 
 public class ControllerManager : MonoBehaviour
 {
+    const int MAX_PLAYERS = 32;
+    private int player_id_counter = 0;
+    PlayerFactory player_factory;
+
+    // Track which Joysticks we've seen before in this session so we can tell new joysticks vs ones that have already been assigned to a Player
+    private Dictionary<int, GameObject> assigned_joystick;
+
     void Awake()
     {
-        // Listen for controller connection events
+        player_factory = GetComponent<PlayerFactory>();
+        assigned_joystick = new Dictionary<int, GameObject>();
+
+        // Subscribe to controller connected events
         ReInput.ControllerConnectedEvent += OnControllerConnected;
+        ReInput.ControllerDisconnectedEvent += OnControllerDisconnected;
+    }
 
-        // Assign each Joystick to a Player initially
-        foreach (Joystick joystick in ReInput.controllers.Joysticks)
-        {
-            if (ReInput.controllers.IsJoystickAssigned(joystick))
-                continue; // Joystick is already assigned
-
-            // Assign Joystick to first Player that doesn't have any assigned
-            AssignJoystickToNextOpenPlayer(joystick);
-        }
+    void Start()
+    {
+        AssignAllJoysticksToSystemPlayer(true);
     }
 
 
     void OnControllerConnected(ControllerStatusChangedEventArgs args)
     {
-        if (args.controllerType != ControllerType.Joystick)
-            return; // skip if this isn't a Joystick
+        if (args.controllerType != ControllerType.Joystick) return;
 
-        AssignJoystickToNextOpenPlayer(ReInput.controllers.GetJoystick(args.controllerId));
+        // Check if this Joystick has already been assigned. If so, just let Auto-Assign do its job.
+        if (assigned_joystick.ContainsKey(args.controllerId)) return;
+
+        // Joystick hasn't ever been assigned before. Make sure it's assigned to the System Player until it's been explicitly assigned
+        ReInput.players.GetSystemPlayer().controllers.AddController<Joystick>(args.controllerId, true);
     }
 
 
-    void AssignJoystickToNextOpenPlayer(Joystick joystick)
+    void OnControllerDisconnected(ControllerStatusChangedEventArgs args)
     {
-        foreach (Player player in ReInput.players.Players)
+        Debug.Log("A controller was disconnected! Name = " + args.name + " Id = " + args.controllerId + " Type = " + args.controllerType);
+
+        RemovePlayer(args.controllerId);
+    }
+
+    void RemovePlayer(int player_id)
+    {       
+        Destroy(assigned_joystick[player_id]);
+        assigned_joystick.Remove(player_id);
+    }
+
+    void AssignAllJoysticksToSystemPlayer(bool removeFromOtherPlayers)
+    {
+        foreach (Joystick j in ReInput.controllers.Joysticks)
         {
-            if (player.controllers.joystickCount > 0)
-                continue; // player already has a joystick
-
-            player.controllers.AddController(joystick, true); // assign joystick to player
-
-            return; // joystick assigned, abort
+            ReInput.players.GetSystemPlayer().controllers.AddController(j, removeFromOtherPlayers);
         }
     }
 
 
-    void OnDestroy()
+    void Update()
     {
-        // Unsubscribe from events
-        ReInput.ControllerConnectedEvent -= OnControllerConnected;
+        // Watch for JoinGame action in System Player
+        if (ReInput.players.GetSystemPlayer().GetButtonDown("SlotDrop"))
+        {
+            AssignNextPlayer();
+        }
+    }
 
-        //ReInput.ControllerDisconnectedEvent -= OnControllerDisconnected;
-        //ReInput.ControllerPreDisconnectEvent -= OnControllerPreDisconnect;
+
+    void AssignNextPlayer()
+    {
+        if (player_id_counter >= MAX_PLAYERS)
+        {
+            Debug.Log("Max player limit already reached!");
+            return;
+        }
+
+        // Get the next Rewired Player Id
+        int rewiredPlayerId = GetNextGamePlayerId();
+
+        // Get the Rewired Player
+        Player rewiredPlayer = ReInput.players.GetPlayer(rewiredPlayerId);
+
+        // Determine which Controller was used to generate the JoinGame Action
+        Player systemPlayer = ReInput.players.GetSystemPlayer();
+        var inputSources = systemPlayer.GetCurrentInputSources("SlotDrop");
+
+        foreach (var source in inputSources)
+        {  
+            if (source.controllerType == ControllerType.Joystick)
+            {
+                AssignJoystickToPlayer(rewiredPlayer, source.controller as Joystick);
+                break;
+            }
+            else
+            { // Custom Controller
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+
+
+    private void AssignJoystickToPlayer(Player player, Joystick joystick)
+    {
+        // Assign the joystick to the Player, removing it from System Player
+        player.controllers.AddController(joystick, true);
+
+        // Mark this joystick as assigned so we don't give it to the System Player again
+        assigned_joystick.Add(joystick.id, player_factory.CreatePlayer(player));
+
+        Debug.Log("Assigned " + joystick.name + " to Player " + player.name);
+       
+    }
+
+
+    private int GetNextGamePlayerId()
+    {
+        return player_id_counter++;
     }
 }
+
