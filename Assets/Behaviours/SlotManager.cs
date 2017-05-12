d74eb8482c28fa609a0f2fb59cd318551e2b5b54
Jdasi <silvacore@hotmail.co.uk>
2017-05-12 16:28:36 +0100

    added slots and slot manager

---
+++ Assets/Behaviours/SlotManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotManager : MonoBehaviour
+{
+    public float min_open_delay = 2;
+    public float max_open_delay = 10;
+
+    private Slot[] slots;
+    private bool random_slot_queued = false;
+
+
+	void Start()
+    {
+		slots = GameObject.FindObjectsOfType<Slot>();
+	}
+	
+
+	void Update()
+    {
+		if (!random_slot_queued)
+        {
+            Invoke("OpenRandomSlot", Random.Range(min_open_delay, max_open_delay));
+            random_slot_queued = true;
+        }
+	}
+
+
+    void CloseAllSlots()
+    {
+        foreach (Slot slot in slots)
+        {
+            slot.Close();
+        }
+    }
+
+
+    // Open a random slot that isn't already open.
+    void OpenRandomSlot()
+    {
+        Slot slot = null;
+
+        do
+        {
+            slot = slots[Random.Range(0, slots.Length)];
+
+        } while (slot.IsOpen());
+
+        slot.Open();
+
+        random_slot_queued = false;
+    }
+
+}
