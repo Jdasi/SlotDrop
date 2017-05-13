75e976358e2c7c359be3b28fc44524fc33cc9da5
Tom Turner <tom3.turner@live.uwe.ac.uk>
2017-05-13 15:56:27 +0100

    Cataclysm triggering

--- Assets/Behaviours/PCManager.cs
+++ Assets/Behaviours/PCManager.cs
@@ -5,12 +5,35 @@ using UnityEngine.UI;
 
 public class PCManager : MonoBehaviour
 {
+    
+    [Header("Indicators")]
     public GameObject virus_indicator;
+    public float anim_duration = 4f;
     public GameObject scan_indicator;
-
     public Slider virus_bar;
     public float virus_bar_increment_percent = 10f;
-    public float anim_duration = 4f;
+
+    [Header("Cataclysm")]
+    public GameObject meteor_prefab;
+    public SpawnAreaCircle meteor_spawn_point;
+    [Range(0,1)]
+    public float meteor_spawn_chance_percentage = 0.25f;
+    public float cataclysm_duration = 10;
+   
+    private bool cataclysm_active = false;
+
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            IncrementVirusBar();
+        }
+        if(cataclysm_active)
+        {
+            RunCataclysm();
+        }
+    }
 
 
     private void Start()
@@ -24,14 +47,15 @@ public class PCManager : MonoBehaviour
     {
         virus_indicator.SetActive(true);
         scan_indicator.SetActive(false);
+        StartCoroutine(EndVirusIndicator());
     }
 
 
     public void IncrementVirusBar()
     {
         virus_bar.value += virus_bar_increment_percent;
-
-        if(virus_bar.value >= virus_bar.maxValue)//if maxed
+        StartVirusIndicator();
+        if (virus_bar.value >= virus_bar.maxValue)//if maxed
         {
             TriggerCataclysm();//start the cataclysm
         }
@@ -40,7 +64,33 @@ public class PCManager : MonoBehaviour
 
     private void TriggerCataclysm()
     {
+        StartCoroutine(EndCataclysm());
+        cataclysm_active = true;
+    }
+
+
+    private void RunCataclysm()
+    {
+        virus_indicator.SetActive(true);
+        scan_indicator.SetActive(false);
+        if (Random.Range(0, 1) < meteor_spawn_chance_percentage)
+        {
+            GameObject meteor_clone = Instantiate(meteor_prefab);
+            Vector2 random_circle_location = Random.insideUnitCircle * meteor_spawn_point.spawn_radius;
+
+            meteor_clone.transform.position = new Vector3(random_circle_location.x,
+                meteor_spawn_point.transform.position.y, random_circle_location.y);
+        }
+    }
+
+
+    private IEnumerator EndCataclysm()
+    {
+        yield return new WaitForSeconds(cataclysm_duration);
+        cataclysm_active = false;
         virus_bar.value = virus_bar.minValue;
+        virus_indicator.SetActive(false);
+        scan_indicator.SetActive(true);
     }
 
 
