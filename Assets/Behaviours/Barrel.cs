d14924e549bfab62b764d52485810e47590fc07c
Jdasi <silvacore@hotmail.co.uk>
2017-05-15 15:58:50 +0100

    added pirates special janky ability

---
+++ Assets/Behaviours/Barrel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrel : MonoBehaviour
+{
+    public GameObject shockwave_particle;
+    public GameObject owner;
+	
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().AddTorque(new Vector3(3, 0, 6));
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Barrel")
+            return;
+
+        if (other.gameObject == owner)
+            return;
+
+        GameObject.FindObjectOfType<AudioManager>().PlayOneShot("explosion");
+
+        CameraShake.instance.ShakeCamera(0.4f, 0.4f);
+
+        Projectile.CreateEffect(shockwave_particle, transform.position, Vector3.zero);
+        RaycastHit[] elems = Projectile.CreateExplosionForce(gameObject, transform.position, 5, 0);
+
+        foreach (var elem in elems)
+        {
+            PlayerController player = elem.collider.GetComponent<PlayerController>();
+
+            if (player == null)
+                continue;
+
+            player.Stun(2.0f);
+        }
+    }
+
+
+}
--- Assets/Behaviours/Projectiles/ProjectileBarrelBomb.cs
+++ Assets/Behaviours/Projectiles/ProjectileBarrelBomb.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileBarrelBomb : MonoBehaviour {
+public class ProjectileBarrelBomb : Projectile
+{
+    public GameObject barrel_prefab;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    protected override void Start()
+    {
+        for (int i = 0; i < 3; ++i)
+        {
+            GameObject bomb = Instantiate(barrel_prefab, owning_player.transform.position + new Vector3(0, 5, 0), Quaternion.identity);
+            bomb.GetComponent<Barrel>().owner = owning_player.gameObject;
+
+            float random_x = Random.Range(-5000, 5000);
+            float random_z = Random.Range(-5000, 5000);
+
+            Vector3 drop_vector = new Vector3(random_x, 10000, random_z);
+
+            bomb.GetComponent<Rigidbody>().AddForce(drop_vector);
+        }
+
+        Destroy(gameObject);
+    }
+
 }
