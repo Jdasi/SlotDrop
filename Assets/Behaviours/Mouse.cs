598ec6b2b5767b722089ccf021c0fc7047fcef4d
Joe da Silva <silvacore@hotmail.co.uk>
2017-05-16 20:27:30 +0100

    moving the mouse object now moves a cursor on the computer screen

---
+++ Assets/Behaviours/Mouse.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    public GameObject hud_mouse;
+    public float sensitivity = 0.1f;
+
+    private Vector3 mouse_pos;
+    private Vector3 prev_mouse_pos;
+
+
+	void Update()
+    {
+		mouse_pos = transform.position;
+
+        if (mouse_pos != prev_mouse_pos)
+        {
+            Vector3 difference = mouse_pos - prev_mouse_pos;
+            difference.y = difference.z;
+            difference.z = 0;
+
+            Debug.Log(difference);
+
+            Vector3 new_mouse_pos = hud_mouse.transform.localPosition + (difference * sensitivity);
+            new_mouse_pos.x = Mathf.Clamp(new_mouse_pos.x, -1.9f, 1.9f);
+            new_mouse_pos.y = Mathf.Clamp(new_mouse_pos.y, -1, 1);
+
+            hud_mouse.transform.localPosition = new_mouse_pos;
+        }
+
+        prev_mouse_pos = mouse_pos;
+	}
+
+
+}
